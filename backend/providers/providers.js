1ad8ef92698e2fb6583405e39f8517471cb005cb
Carolina Román-Salgado <carol.roman.salgado@gmail.com>
2025-03-06 16:21:32 -0500

    Add providers.csv and implement API endpoint to fetch providers data

---
+++ backend/providers/providers.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import { loadCSV } from "../client.js";
+import { sendApiResponse } from "../utils.js";
+
+const router = Router();
+
+export const getProviderById = async (req, res) => {
+  const allProviders = await loadCSV("providers.csv", "providers");
+
+  return sendApiResponse(res, 200, allProviders);
+};
+
+router.get("", getProviderById);
+
+export default router;
