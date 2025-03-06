b4accb10f2158b9b45a000b8a0c1749fb9fdbc90
Carolina Román-Salgado <carol.roman.salgado@gmail.com>
2025-03-06 18:07:19 -0500

    Add path alias configuration to Vite resolve options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // So imports don't have to use '../../../'
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
