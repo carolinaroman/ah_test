b16a5a19c3dae097eeff48adbf613cc8ed0ceffd
Carolina Román-Salgado <carol.roman.salgado@gmail.com>
2025-03-08 16:33:30 -0500

    Removed unnecesary code

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,11 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
 
 import routes from "./routes.js";
 import { sendApiResponse } from "./utils.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-dotenv.config({ path: join(__dirname, "../.env") });
+dotenv.config();
 
 const app = express();
 
